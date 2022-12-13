5bc133726524b7b93c0b4e4b9aae37f37fa7e258
dominik_fin <dominik.finzer@stud.hslu.ch>
2022-12-13 17:12:31 +0100

    improved functions

--- functions.py
+++ functions.py
@@ -85,8 +85,19 @@ def get_top_n_bigram(corpus, n=None):
     return words_freq[:n]
 
 def anonymizer_de(list):
+    """
+    Anonymize a list of text comments.
     
-    anonymized_text_list = []
+    This function uses a natural language processing (NLP) engine and an anonymizer engine to identify and anonymize personal information in a list of text comments. The function supports German language comments, and can recognize entities such as person names, email addresses, phone numbers, credit card numbers, and IBAN codes.
+    
+    Args:
+        list: A list of strings containing text comments to be anonymized.
+    
+    Returns:
+        A list of strings containing the anonymized text comments.
+    """
+
+    
     
     #Create configuration containing engine name and models
     configuration = {
@@ -101,33 +112,43 @@ def anonymizer_de(list):
     # # for finding phones, passport numbers, etc.
     analyzer = AnalyzerEngine(nlp_engine=nlp_engine,
                               supported_languages=["de"])
+
+    # Create the anonymizer
+    anonymizer = AnonymizerEngine()
+
+    # Anonymize each comment in the list
+    anonymized_text_list = [
+        anonymizer.anonymize(text=comment,
+                             analyzer_results=analyzer.analyze(text=comment,
+                                                              language='de',
+                                                              entities=["PERSON","EMAIL_ADDRESS","PHONE_NUMBER","CREDIT_CARD","IBAN_CODE"])).text
+        if isinstance(comment, str) else None
+        for comment in list
+    ]
     
-    for comment in list:
-        if isinstance(comment, str):
-            results = analyzer.analyze(text=comment,
-                           language='de',entities=["PERSON","EMAIL_ADDRESS","PHONE_NUMBER","CREDIT_CARD","IBAN_CODE"])
-            anonymizer = AnonymizerEngine()
-            anonymized_text = anonymizer.anonymize(text=comment, analyzer_results=results).text
-            anonymized_text_list.append(anonymized_text)
-        else:
-            anonymized_text = np.NaN
-            anonymized_text_list.append(anonymized_text)
-            
-            
-        
     return anonymized_text_list
 
 
-def find_extract_ner_entities_list(list,entitie):
+def find_extract_ner_entities_list(lst, entitie):
+    """
+    Find and extract named entities from a list of strings using natural language processing.
     
-    ### This function takes a list of strings and one NER entitie (e.g."EMAIL_ADDRESS","PHONE_NUMER",...)as input.It outputs the detected NER Enteties as a List and if nothing was found inserts NONE.
+    This function uses a specified NLP engine and language model to detect named entities in a list of strings,
+    and returns a list of the detected entities. If the input is not a string, None is returned instead.
     
-    results_list = []
-    
-    #Create configuration containing engine name and models
+    Args:
+        lst: A list of strings to process.
+        entitie: The named entity type to look for (e.g. "person", "organization", "location", etc.)
+        
+    Returns:
+        A list of detected entities, or None if the input is not a string.
+    """
+
+    # Create configuration containing engine name and models
     configuration = {
-    "nlp_engine_name": "spacy",
-    "models": [{"lang_code": "de", "model_name": "de_core_news_lg"}],}
+        "nlp_engine_name": "spacy",
+        "models": [{"lang_code": "de", "model_name": "de_core_news_lg"}],
+    }
     
     # Create NLP engine based on configuration
     provider = NlpEngineProvider(nlp_configuration=configuration)
@@ -135,27 +156,27 @@ def find_extract_ner_entities_list(list,entitie):
     
     # the languages are needed to load country-specific recognizers 
     # # for finding phones, passport numbers, etc.
-    analyzer = AnalyzerEngine(nlp_engine=nlp_engine,
-                              supported_languages=["de"])
+    analyzer = AnalyzerEngine(nlp_engine=nlp_engine, supported_languages=["de"])
     
-    for comment in list:
-        if isinstance(comment, str):
-            
-            results = analyzer.analyze(text=comment,
-                           language='de', entities=[entitie]
-                           )
-            
-            detected_entities = [(comment[res.start:res.end]) for res in results]
-            results_list.append(detected_entities)
-            
-        else:
-            detected_entities = None
-            results_list.append(detected_entities)
-            
-    # Replace empty List from List with None using list comprehension
+    # Create a list comprehension that processes each string in the input list and
+    # appends the detected entities to the results list. If the input is not a string,
+    # None is appended to the results list.
+    results_list = [
+        None if not isinstance(comment, str)
+        else [(comment[res.start:res.end]) for res in analyzer.analyze(text=comment, language="de", entities=[entitie])]
+        for comment in lst
+    ]
+
+    # Replace empty lists in the results list with None.
     results_list = [None if not x else x for x in results_list]
 
     return results_list
+
+
+def check_column_values(df, col1, col2):
+    # Check if either of the two columns contains a non-null value
+    result = (df[col1].notnull() | df[col2].notnull()).tolist()
+    return result
     
     
     
